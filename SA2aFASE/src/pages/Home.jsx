eecf544229d5f72a1a2f50893d86776fbd6d917c
THOMAZ DE ANDRADE BERGENTAL FILHO <thomaz_bergental-@estudante.sesisenai.org.br>
2024-10-28 17:05:28 -0300

    ta indo

--- SA2aFASE/src/pages/Home.jsx
+++ SA2aFASE/src/pages/Home.jsx
@@ -19,25 +19,33 @@ function Home() {
         
         <div className="div-2">
          
-          <p>
+          <p className="p1">
           Transforme suas noites em momentos de descanso profundo.
           </p>
-          <p className="p2">Registre e acompanhe cada detalhe do seu sono para dias mais produtivos e energizados.</p>
+
+          <p className="p2">
+          Registre e acompanhe cada detalhe do seu sono para dias mais produtivos e energizados.
+          </p>
+         
           <div className="div-4">
+
             <button className="btn-2">Comece aqui</button>
+
           </div>
+
         </div>
 
         <div className="div-3">
-          <img src="public/images/landing.png" alt="foguete"/>
+          <img src="public/images/foguete.png" alt="foguete"/>
         </div>
       </div>
 
       <div className="div-5">
         
-        <label>INSTAGRAM</label>
-        <label>FACEBOOK</label>
-        <label>EMAIL</label>
+        <button className="buttons">Email</button>
+        <button className="buttons">Twitter</button>
+        <button className="buttons">Youtube</button>
+        
       </div>
     </div>
   );
