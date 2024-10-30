63ecd49468f69b967e581b7a6b299bcb266ee810
guilhxrme05 <gguihurtado@gmail.com>
2024-10-30 15:51:11 -0300

    calendario

--- SA2aFASE/src/pages/Generica.jsx
+++ SA2aFASE/src/pages/Generica.jsx
@@ -1,12 +1,34 @@
-import Navbar from "../components/Navbar"
+import React, { useState } from 'react';
+import Navbar from "../components/Navbar";
+import './Calendario.css';
 
-function Generica() {
+function WeeklyCalendar() {
+  const [weekNumber, setWeekNumber] = useState(1);
+  const daysOfWeek = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado", "Domingo"];
+
+
+  const goToNextWeek = () => setWeekNumber(weekNumber + 1);
+  const goToPreviousWeek = () => setWeekNumber(weekNumber > 1 ? weekNumber - 1 : 1);
+
   return (
     <div>
-        <Navbar />
-      
+      <Navbar />
+      <div className="weekly-calendar-container">
+        <div className="header">
+          <button className='voltaSemana' onClick={goToPreviousWeek}>&lt;</button>
+          <h2 className='semanaTitulo'>Semana {weekNumber}</h2>
+          <button className='ProximaSemana' onClick={goToNextWeek}>&gt;</button>
+        </div>
+        <div className="week">
+          {daysOfWeek.map((day, index) => (
+            <div key={index} className="day">
+              <h3>{day}</h3>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Generica
+export default WeeklyCalendar;
