55f9caa017ccf1ad095a5f6cf165c17274a6e90a
guilhxrme05 <gguihurtado@gmail.com>
2024-11-19 15:18:33 -0300

    card pronto

--- SA2aFASE/src/components/EditCard.jsx
+++ SA2aFASE/src/components/EditCard.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./EditCard.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faThumbsUp, faThumbsDown } from "@fortawesome/free-solid-svg-icons";
 
 function EditCard({ day, onClose }) {
+  const [selected, setSelected] = useState(null);
+
   return (
     <div className="edit-card-overlay">
       <div className="edit-card">
@@ -29,12 +33,40 @@ function EditCard({ day, onClose }) {
           </div>
           <p className="avaliacao">AVALIAÇÃO</p>
           <div className="avaliacao-buttons">
-            <p>thumbsup</p>
-            <p>thumbsdown</p>
+            <FontAwesomeIcon
+              icon={faThumbsUp}
+              style={{
+                color: "#008509",
+                cursor: "pointer",
+                fontSize: "60px",
+              }}
+              onClick={() => setSelected("up")}
+            />
+            <FontAwesomeIcon
+              icon={faThumbsDown}
+              style={{
+                color: "#a80000",
+                cursor: "pointer",
+                fontSize: "60px",
+              }}
+              onClick={() => setSelected("down")}
+            />
+          </div>
+          <div className="feedback-container">
+            {selected === "up" && (
+              <p className="feedback-message" style={{ color: "#008509" }}>
+                Você escolheu uma avaliação positiva!
+              </p>
+            )}
+            {selected === "down" && (
+              <p className="feedback-message" style={{ color: "#a80000" }}>
+                Você escolheu uma avaliação negativa!
+              </p>
+            )}
           </div>
         </div>
         <div className="edit-card-actions">
-        <button className="btn-save" onClick={onClose}>
+          <button className="btn-save" onClick={onClose}>
             Finalizar
           </button>
         </div>
